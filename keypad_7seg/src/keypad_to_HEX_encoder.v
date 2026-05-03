80689267c10236cbfc024415f7378e89c1b04cc6
pou9243 <parkonyou9243@gmail.com>
2026-05-04 00:00:28 +0900

    Rewrote the keypad-to-HEX-encoder logic so that it can work with the redesigned debouncer logic.

--- keypad_7seg/src/keypad_to_HEX_encoder.v
+++ keypad_7seg/src/keypad_to_HEX_encoder.v
@@ -27,8 +27,10 @@ input[3:0] col,
 output reg[3:0] scan_value,
 output reg scan_valid
     );
+    
+    reg scan_pulse;
     always @(posedge clk) begin
-        scan_valid <= 1'b1;
+        scan_pulse <= 1'b1;
         case({row, col})
             8'b1000_1000: scan_value <= 4'd1;
             8'b1000_0100: scan_value <= 4'd2;
@@ -49,7 +51,43 @@ output reg scan_valid
             8'b0001_0100: scan_value <= 4'd0;
             8'b0001_0010: scan_value <= 4'd15;
             8'b0001_0001: scan_value <= 4'd13;
-            default: begin scan_value <= scan_value; scan_valid <= 1'b0; end
+            default: begin scan_value <= scan_value; scan_pulse <= 1'b0; end
+        endcase
+    end
+    
+    localparam IDLE = 2'b00;
+    localparam DETECTED = 2'b01;
+    localparam RELEASE_WAIT = 2'b10;
+    reg[1:0] release_cnt = 2'd0;
+    reg[1:0] state = IDLE;
+    always @(posedge clk) begin
+        case (state)
+            IDLE: begin
+                if (scan_pulse) begin
+                    state <= DETECTED;
+                    scan_valid <= 1;
+                end else begin
+                    scan_valid <= 0;
+                end
+            end
+            
+            DETECTED: begin
+                release_cnt <= 2'd0;
+                state <= RELEASE_WAIT;
+            end
+            
+            RELEASE_WAIT: begin
+                if (scan_pulse) begin
+                    release_cnt <= 2'd0;
+                end else begin
+                    if (release_cnt == 2'd3) begin
+                        scan_valid <= 0;
+                        state <= IDLE;
+                    end else begin
+                        release_cnt <= release_cnt + 2'd1;
+                    end
+                end
+            end
         endcase
     end
 endmodule
